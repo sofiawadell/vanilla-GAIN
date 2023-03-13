5373b02c3c136d6e76c3566835b4d48058bde846
Hanna <ha6303an-s@student.lu.se>
2023-03-13 12:13:02 -0300

    new datasets

--- test.py
+++ test.py
@@ -1,16 +1,30 @@
 import pandas as pd
+from datasets import datasets
+
+# Determine dataset, missingness and mode (test/train)
+all_datasets = ["mushroom", "news", "credit", "letter", "bank"]
+all_missingness = [10, 30, 50, 70]
 
 
-data_name = "news"
+for data_name in all_datasets:
+    for missingness in all_missingness:
+        target = datasets[data_name]["target"]
+        filename_train = '{}{}_{}_{}.csv'.format('one_hot_train_data/one_hot_', data_name, 'train', missingness)
+        filename_test = '{}{}_{}_{}.csv'.format('one_hot_test_data/one_hot_', data_name, 'test', missingness)
 
-df_train = pd.read_csv('train_data/'+data_name+'_train.csv')
-df_test = pd.read_csv('test_data/'+data_name+'_test.csv')
+        df_train = pd.read_csv(filename_train)
+        df_test = pd.read_csv(filename_test)
 
-size_train, dim_train = df_train.shape
-size_test, dim_test = df_test.shape
+        df_train = df_train.drop(target, axis=1)
+        df_test = df_test.drop(target, axis=1)
 
-print("Train: " + str(size_train))
-print("Test: " + str(size_test))
+        # Save as csv
+        filename_train = '{}{}_{}_{}.csv'.format('one_hot_train_data_wo_target/one_hot_', data_name, 'train', missingness)
+        df_train.to_csv(filename_train, index=False)
 
-print(dim_train)
-print(dim_test)
+        filename_test = '{}{}_{}_{}.csv'.format('one_hot_test_data_wo_target/one_hot_', data_name, 'test', missingness)
+        df_test.to_csv(filename_test, index=False)
+
+
+
+
